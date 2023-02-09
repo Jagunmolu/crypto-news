e4fde996bf584f15da36f9e8d3b1f76c7e617339
Jagunmolu <kolawolejonathan@gmail.com>
2023-02-09 16:40:04 +0100

    changed debug settings to true

--- crypto_news_site/settings.py
+++ crypto_news_site/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-tk2#10^@l))=ilc0e@g1+@rz*84td8n^tt!60*lvn51qw_j#*3"
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://crypto-news-production.up.railway.app']
 
